94d445d883399dce3e4c3fe2a1cc8cfd2782560d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "api_management_api_tag_descriptions_id" {
+  description = "Map of id values across all api_management_api_tag_descriptions, keyed the same as var.api_management_api_tag_descriptions"
+  value       = { for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : k => v.id }
+}
 output "api_management_api_tag_descriptions_api_tag_id" {
   description = "Map of api_tag_id values across all api_management_api_tag_descriptions, keyed the same as var.api_management_api_tag_descriptions"
   value       = { for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : k => v.api_tag_id }

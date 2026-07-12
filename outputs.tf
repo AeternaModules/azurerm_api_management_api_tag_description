e0f03068ac5b310c44a257834e1fa233c21c05e8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "api_management_api_tag_descriptions" {
-  description = "All api_management_api_tag_description resources"
-  value       = azurerm_api_management_api_tag_description.api_management_api_tag_descriptions
-}
 output "api_management_api_tag_descriptions_api_tag_id" {
-  description = "List of api_tag_id values across all api_management_api_tag_descriptions"
-  value       = [for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : v.api_tag_id]
+  description = "Map of api_tag_id values across all api_management_api_tag_descriptions, keyed the same as var.api_management_api_tag_descriptions"
+  value       = { for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : k => v.api_tag_id }
 }
 output "api_management_api_tag_descriptions_description" {
-  description = "List of description values across all api_management_api_tag_descriptions"
-  value       = [for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : v.description]
+  description = "Map of description values across all api_management_api_tag_descriptions, keyed the same as var.api_management_api_tag_descriptions"
+  value       = { for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : k => v.description }
 }
 output "api_management_api_tag_descriptions_external_documentation_description" {
-  description = "List of external_documentation_description values across all api_management_api_tag_descriptions"
-  value       = [for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : v.external_documentation_description]
+  description = "Map of external_documentation_description values across all api_management_api_tag_descriptions, keyed the same as var.api_management_api_tag_descriptions"
+  value       = { for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : k => v.external_documentation_description }
 }
 output "api_management_api_tag_descriptions_external_documentation_url" {
-  description = "List of external_documentation_url values across all api_management_api_tag_descriptions"
-  value       = [for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : v.external_documentation_url]
+  description = "Map of external_documentation_url values across all api_management_api_tag_descriptions, keyed the same as var.api_management_api_tag_descriptions"
+  value       = { for k, v in azurerm_api_management_api_tag_description.api_management_api_tag_descriptions : k => v.external_documentation_url }
 }
 
